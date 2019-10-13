43c627f1a464a92c24e9cbee62686a2c2ec255f9
hakue1990 <adam.haldas@gmail.com>
2019-10-13 21:02:40 +0200

    add new nanan

---
+++ Podstawy programowania/asd/nowy test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj wysokosc:");
+            string h = Console.ReadLine();
+            Console.WriteLine("podaj szerokosc: ");
+            string w = Console.ReadLine();
+
+            int h1 = int.Parse(h);
+            int w1 = int.Parse(w);
+
+            for (int i = 0; i < h1; i++)
+
+            {
+                for (int j = 0; j < w1; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("#");
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+}
