eeb9713bdb8e437343b35a21abbd5a97f902cfd6
hakue1990 <adam.haldas@gmail.com>
2019-11-14 00:05:21 +0100

    add new project validation input data

--- Podstawy programowania/Exceptions3/Program.cs
+++ Podstawy programowania/Exceptions3/Program.cs
@@ -22,70 +22,118 @@ namespace Exceptions3
              */
 
 
-            while (true)
+            //while (true)
+            //{
+            /*
+            try
+            {
+                string tabindex; // deklaracja dlugosci tablicy
+                Console.WriteLine("\nPodaj ile ma byc człornków rodziny: ");
+                tabindex = Console.ReadLine(); // pobranie dlugosci tablicy od uzytkownika
+                int newIntNumber = Int32.Parse(tabindex);
+                uint number;
+                string[] answer = new string[newIntNumber];
+                double srednia = 0.0;
+                for (int i = 0; i < newIntNumber; i++)
+                {
+                    Console.WriteLine("Podaj wiek członka rodziny: {0} ", i + 1);
+                    answer[i] = Console.ReadLine();
+                    int newUserAge1 = int.Parse(answer[i]);
+                    srednia += newUserAge1;
+                }
+                for (int j = 0; j < newIntNumber; j++)
+                {
+                    Console.WriteLine("wiek {1} członka rodziny to: {0}", answer[j], j + 1);
+                }
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Srednia wieku userów to {0}", srednia / newIntNumber);
+                    Console.ResetColor();
+                Console.WriteLine("Jaki jest wiek ostatniego członka rodziny?");
+        Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Wiek ostatniego członka rodziny to: {0}", answer[answer.Length - 1]);
+                number = uint.Parse(tabindex);
+                break;
+
+            }
+            catch (FormatException)
             {
 
-                try
-                {
-                    string tabindex; // deklaracja dlugosci tablicy
-                    Console.WriteLine("\nPodaj ile ma byc człornków rodziny: ");
-                    tabindex = Console.ReadLine(); // pobranie dlugosci tablicy od uzytkownika
-                    int newIntNumber = Int32.Parse(tabindex);
-                    uint number;
-                    string[] answer = new string[newIntNumber];
-                    double srednia = 0.0;
-                    for (int i = 0; i < newIntNumber; i++)
-                    {
-                        Console.WriteLine("Podaj wiek członka rodziny: {0} ", i + 1);
-                        answer[i] = Console.ReadLine();
-                        int newUserAge1 = int.Parse(answer[i]);
-                        srednia += newUserAge1;
-                    }
-                    for (int j = 0; j < newIntNumber; j++)
-                    {
-                        Console.WriteLine("wiek {1} członka rodziny to: {0}", answer[j], j + 1);
-                    }
-                    Console.ForegroundColor = ConsoleColor.DarkBlue;
-                    Console.WriteLine("Srednia wieku userów to {0}", srednia / newIntNumber);
-                        Console.ResetColor();
-                    Console.WriteLine("Jaki jest wiek ostatniego członka rodziny?");
-            Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Wiek ostatniego członka rodziny to: {0}", answer[answer.Length - 1]);
-                    number = uint.Parse(tabindex);
-                    break;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Podaj liczbe w poprawnym formacie!");
+                Console.ResetColor();
 
-                }
-                catch (FormatException)
-                {
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Podaj liczbe w poprawnym formacie!");
-                    Console.ResetColor();
-
-                }
-                catch (OverflowException)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("wprowadzona liczba jest poza dopuszczalnym zakresem");
-                    Console.ResetColor();
-                }
-                catch (ArgumentNullException)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Wartość nie mozebyc pusta");
-                    Console.ResetColor();
-                }
-                catch (Exception e)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Error {0}", e.Message);
-                    Console.ResetColor();
-                }
             }
-            Console.ReadKey();
-            Console.ReadKey();
+            catch (OverflowException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("wprowadzona liczba jest poza dopuszczalnym zakresem");
+                Console.ResetColor();
+            }
+            catch (ArgumentNullException)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Wartość nie mozebyc pusta");
+                Console.ResetColor();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Error {0}", e.Message);
+                Console.ResetColor();
+            }
+        }
+        // *****************************************   TROJKA PITAGOREJSKA  *************************************************************************
+        */
+            //Console.WriteLine("Podaj trzy cyfry:");
+
+            //Console.WriteLine("Podaj a");
+            //var a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Podaj b");
+            //var b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Podaj c");
+            //var c = int.Parse(Console.ReadLine());
+            //if ((a * a + b * b == c * c))
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine("to jest trojka pitagorejska");
+            //}
+            //else
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("to nie jest trojka pitagorejska");
+            //}
+            //Console.ResetColor();
+
+            //}
+            //Console.WriteLine("input your age");
+            //var ageAsAString = Console.ReadLine();
+            //int age;
+            //bool parseSucess = int.TryParse(ageAsAString, out age);
+            //if (parseSucess)
+            //{
+            //    Console.WriteLine("your age is: {0}", age);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("error");
+            //}
+            //Console.ReadKey();
+            Console.WriteLine("Podaj dlugosc tablicy");
+            var index = int.Parse(Console.ReadLine());
+            string[] tablica = new string[index];
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Console.WriteLine("podaj zawartosc tablicy");
+                tablica[i] = Console.ReadLine();
+            }
+            for (int j = 0; j < tablica.Length; j++)
+            {
+                Console.WriteLine("elementy tablicy to: {0}  \t", tablica[j]
+                    );
+            }
             Console.ReadKey();
         }
     }
 }
+
 
---
+++ Podstawy programowania/pressQValidation/TryParse/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goodCounter = 0, badCounter = 0;
+            string input = "";
+            do
+            {
+                Console.WriteLine("Enter a number 0 - 10. press q to Quit");
+                input = Console.ReadLine();
+                int number;
+                if (!input.ToUpper().Equals("Q"))
+                {
+                if (int.TryParse(input, out number))
+                {
+                    if (number >= 0 && number <=10)
+                    {
+                        goodCounter++;
+                    }else
+                    {
+                        badCounter++;
+                    }
+                }
+
+                }
+
+            } while (!input.ToUpper().Equals("Q"));
+            Console.WriteLine("valid counter: {0}", goodCounter);
+            Console.WriteLine("invalid counter: {0}", badCounter);
+            Console.ReadKey();
+        }
+    }
+}
+
+            
+            
+            
+
+
