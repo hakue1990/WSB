9f2b87486cdeb8e7952877a4cb09a6ccc704f406
hakue1990 <48098101+hakue1990@users.noreply.github.com>
2020-03-23 19:55:55 +0100

    Add files via upload

---
+++ Podstawy programowania/Ciąg Fibonacciego/Ciąg Fibonacciego/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciąg_Fibonacciego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("który wyraz ciągu chcesz obliczyć?");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} wyrazciągu to: {1}", n, fib(n));
+
+            Console.ReadKey();
+        }
+public static int fib(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }else
+            {
+                return fib(n - 1) + fib(n - 2);
+            }
+           
+        }
+    }
+}
